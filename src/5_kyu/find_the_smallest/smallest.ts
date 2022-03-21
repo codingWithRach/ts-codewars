1af03555ef19cd4ff099407ada6087b7eedd914f
Rachel Miles <rachmiles@gmail.com>
2022-03-21 09:36:14 +0000

    Reworked to remove duplication of functionality

--- src/5_kyu/find_the_smallest/smallest.ts
+++ src/5_kyu/find_the_smallest/smallest.ts
@@ -1,92 +1,113 @@
 export function smallest(n: number): number[] {
   // if the lowest digit is already in position 0, ignore it and focus on the remaining digits
-  // repeat until we find the lowest digit in the remaining digits that is not in position 1
+  // repeat until we find the lowest digit in the remaining digits that is not in position 0
   const numArray = [...n.toString()].map((char) => Number(char));
   let minNum = Math.min(...numArray);
   let pos = numArray.indexOf(minNum);
 
-  const copyArray = numArray.slice();
+  const remainingArray = numArray.slice();
   while (pos === 0) {
-    copyArray.shift();
-    minNum = Math.min(...copyArray);
-    pos = copyArray.indexOf(minNum);
+    remainingArray.shift();
+    minNum = Math.min(...remainingArray);
+    pos = remainingArray.indexOf(minNum);
   }
-  if (copyArray.length === 0) {
+  const offset = numArray.length - remainingArray.length;
+  const startArray = numArray.slice(0, offset);
+
+  // if there are no remaining digits, the number was already in the correct order
+  if (remainingArray.length === 0) {
     return [n, 0, 0];
   }
-  const offset = numArray.length - copyArray.length;
 
+  // Inline function to determine whether the direction of the moved digit can be reversed to give the same result
   const dirCanReverse = (arr: Array<number>): boolean => {
     return (
       Number(
         arr
           .slice(1, arr.length)
-          .concat([copyArray[0]], copyArray.slice(arr.length))
+          .concat([remainingArray[0]], remainingArray.slice(arr.length))
           .join("")
       ) ===
       Number(
-        [copyArray[arr.length - 1]]
+        [remainingArray[arr.length - 1]]
           .concat(arr.slice(0, arr.length - 1), arr.slice(arr.length))
           .join("")
       )
     );
   };
 
-  // if the lowest digit is in position 1 (and there are no other instances of the digit),
-  // then move out the digit in position 0 and place it to the left of the first number it
-  // encounters that is larger than it
-  // if not found, move to the end
-  if (pos === 1) {
-    if (
-      copyArray.indexOf(minNum, pos + 1) === -1 ||
-      copyArray[0] > copyArray[2]
-    ) {
-      const digitToMove = copyArray[0];
-      let posToMoveTo = copyArray.findIndex((num) => num > digitToMove);
-      if (posToMoveTo === -1) {
-        posToMoveTo = copyArray.length;
-      }
-      const copyRearranged = copyArray
+  // inline function to check for duplicates
+  // if the digit in the position we're moving to is the same as the digit in the previous position
+  // we can effectively move it to the previous position
+  const adjPosForDuplicates = (arr: Array<number>, pos: number) => {
+    while (pos > 0 && arr[pos] === arr[pos - 1]) pos--;
+    return pos;
+  };
+
+  // initialise variables
+  let resultArray = [];
+  let posToMove = 0;
+  let posToMoveTo = 0;
+
+  // move the digit in position 0 if the lowest digit is in position 1 and either:
+  // - there are no other instances of the digit, or
+  // - the digit at the start is greater than the digit after the lowest
+  if (
+    pos === 1 &&
+    (remainingArray.indexOf(minNum, pos + 1) === -1 ||
+      remainingArray[0] > remainingArray[2])
+  ) {
+    posToMove = offset;
+
+    // the digit will be moved to the left of the first digit larger than it
+    posToMoveTo = remainingArray.findIndex((num) => num > remainingArray[0]);
+    // if a digit larger than the one being moved is not found, the digit will be moved to the end
+    if (posToMoveTo === -1) {
+      posToMoveTo = remainingArray.length;
+    }
+
+    // populate a new array starting with the bit that was already in the correct order, and
+    // followed by a rearrangment of the remaining numbers with the first digit moved
+    resultArray = startArray.concat(
+      remainingArray
         .slice(1, posToMoveTo)
-        .concat([digitToMove], copyArray.slice(posToMoveTo));
-      const newArray = numArray.slice(0, offset).concat(copyRearranged);
-      posToMoveTo = posToMoveTo + offset - 1;
-      while (
-        posToMoveTo > 0 &&
-        newArray[posToMoveTo] === newArray[posToMoveTo - 1]
-      ) {
-        posToMoveTo -= 1;
-      }
-      return [Number(newArray.join("")), offset, posToMoveTo];
+        .concat([remainingArray[0]], remainingArray.slice(posToMoveTo))
+    );
+
+    // calculate the position to move to relative to the full number (rather than just the remaining digits),
+    // and adjust it if duplicate digits are found
+    posToMoveTo = adjPosForDuplicates(resultArray, posToMoveTo + offset - 1);
+  } else {
+    // otherwise, find the instance of the smallest digit such that it's the first instance in the last
+    // block of repeated instances
+    const revArray = remainingArray.slice().reverse();
+    let revPos = revArray.indexOf(minNum);
+    while (revArray[revPos] === minNum) {
+      revPos += 1;
+    }
+    const remainingPosToMove = remainingArray.length - revPos;
+
+    // populate a new array starting with the bit that was already in the correct order, and
+    // followed by a rearrangment of the remaining numbers with the appropriate smallest one
+    // moved to position 0
+    resultArray = startArray.concat(
+      [minNum].concat(
+        remainingArray.slice(0, remainingPosToMove),
+        remainingArray.slice(remainingPosToMove + 1)
+      )
+    );
+
+    // calculate the position to move to relative to the full number (rather than just the remaining digits),
+    // and adjust it if duplicate digits are found
+    posToMoveTo = adjPosForDuplicates(resultArray, offset);
+
+    // calculate the position being moved relative to the full number (rather than just the remaining digits)
+    posToMove = remainingPosToMove + offset;
+
+    // if the direction of the move can be reversed, the solution is the one with the smallest index
+    if (dirCanReverse(remainingArray)) {
+      [posToMove, posToMoveTo] = [posToMoveTo, posToMove];
     }
   }
-
-  // otherwise, find the instance of the smallest digit such that it's the first instance in the last
-  // block of repeated instances
-  const revArray = copyArray.slice().reverse();
-  let revPos = revArray.indexOf(minNum);
-  while (revArray[revPos] === minNum) {
-    revPos += 1;
-  }
-  const posToMove = copyArray.length - revPos;
-  const copyRearranged = [minNum].concat(
-    copyArray.slice(0, posToMove),
-    copyArray.slice(posToMove + 1)
-  );
-  const newArray = numArray.slice(0, offset).concat(copyRearranged);
-  let posToMoveTo = offset;
-  while (
-    posToMoveTo > 0 &&
-    newArray[posToMoveTo] === newArray[posToMoveTo - 1]
-  ) {
-    posToMoveTo -= 1;
-  }
-  if (dirCanReverse(copyArray)) {
-    return [
-      Number(newArray.join("")),
-      Math.min(posToMove + offset, posToMoveTo),
-      Math.max(posToMove + offset, posToMoveTo),
-    ];
-  }
-  return [Number(newArray.join("")), posToMove + offset, posToMoveTo];
+  return [Number(resultArray.join("")), posToMove, posToMoveTo];
 }
